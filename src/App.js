f8d911add49037adf9cd6a0cf2f3d80216543499
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-10-31 17:14:29 -0400

    adds homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import HomePage from './Components/HomePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+  state={
+    products:[],
+    token:"",
+    username:"",
+    current_cart:{
+      id:0,
+      orders:[]
+    },
+    past_orders:[]
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/products")
+    .then(resp=>resp.json())
+    .then(products=>{
+      console.log("this is products", products)
+      this.setState({
+        products: products
+      })
+    })
+  }
+    // if(localStorage.token){
+    //   fetch("http://localhost:3000/keep_loggen_in")
+    // }
+  
+
+
+
+
+  render(){
+    return (
+      <div className="App">
+      <HomePage/>
+      </div>
+    )
+  }
 }
 
 export default App;
